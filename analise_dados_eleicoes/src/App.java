dbab5e02c1a2bd0642b6a38e89cc1dd2a1065916
Marcos Vinícius de S. Silva <139233609+mvssilva@users.noreply.github.com>
2023-10-30 10:32:25 -0300

    Programa em Java

---
+++ analise_dados_eleicoes/src/App.java
@@ -0,0 +1,17 @@
+import eleicao.Eleicao;
+import relatorio.Relatorio;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        if(args.length != 4){
+            System.out.println("ERROR : Quantidade incorreta de parametros!");
+            System.out.println("java -jar deputados.jar <opcao_de_cargo> <caminho_arquivo_candidatos>"+
+                    "<caminho_arquivo_votacao> <data>");
+            System.exit(1);
+        }
+        Eleicao eleicao = new Eleicao(args[0], args[1], args[2]);       
+        System.setProperty("file.encoding", "UTF-8");
+        Relatorio.gerarRelatorio(eleicao, args[0], args[3]);
+    }
+}
